1d266f73ef73aabda6d5ce5baa2b4934f938011b
kangjmbu <84979257+kangjmbu@users.noreply.github.com>
2022-02-02 18:16:13 +0800

    Create 06_从尾到头打印链表.cpp

---
+++ leecode_work/leecode_work/06_从尾到头打印链表.cpp
@@ -0,0 +1,53 @@
+#include<vector>
+#include<iostream>
+#include<string>
+#include<deque>
+using namespace std;
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(int x) : val(x), next(NULL) {}
+	
+};
+class Solution {
+public:
+	vector<int> reversePrint(ListNode* head) {
+		deque<int> v;
+		while (head != NULL)
+		{
+			v.push_front(head->val);
+			head = head->next;
+
+		}
+		vector<int> vv;
+
+		for (int num : v)
+		{
+			vv.push_back(num);
+		}
+		return vv;
+	}
+};
+void test06()
+{
+	Solution s;
+	ListNode L1(1);
+	
+	ListNode L2(3);
+	ListNode L3(2);
+	L1.next = &L2;
+	L2.next = &L3;
+	
+	vector<int> bb = s.reversePrint(&L1);
+	cout << bb.size() << endl;
+
+
+
+
+}
+int main()
+{
+	test06();
+	system("pause");
+	return 0;
+}
