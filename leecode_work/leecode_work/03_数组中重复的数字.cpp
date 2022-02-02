6f27174f6ce8b1bfb4793c632b90ed502fa82f11
kangjmbu <84979257+kangjmbu@users.noreply.github.com>
2022-02-02 14:51:53 +0800

    Create 03_数组中重复的数字.cpp

---
+++ leecode_work/leecode_work/03_数组中重复的数字.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+using namespace std;
+
+class Solution {
+public:
+	int findRepeatNumber(vector<int>& nums) {
+		unordered_map<int, bool> map;
+		for (int num : nums) {
+			if (map[num]) return num;
+			map[num] = true;
+		}
+		return -1;
+
+	}
+};
+void test03()
+{
+	Solution s;
+	vector<int> uu;
+	int u[7] = { 2, 3, 1, 0, 2, 5, 3 };
+	for (int i = 0; i < 7; i++)
+	{
+		uu.push_back(u[i]);
+
+	}
+	int j = s.findRepeatNumber(uu);
+	cout << j << endl;
+	
+}
+int main()
+{
+	test03();
+	system("pause");
+	return 0;
+}
