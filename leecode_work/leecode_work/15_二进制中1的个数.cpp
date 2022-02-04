fc5f1e4420ed84afd3d42d63378ab9cc04b2fc9f
kangjmbu <84979257+kangjmbu@users.noreply.github.com>
2022-02-04 16:08:09 +0800

    Create 15_二进制中1的个数.cpp

---
+++ leecode_work/leecode_work/15_二进制中1的个数.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+#include<string>
+
+using namespace std;
+
+
+
+
+class Solution {
+public:
+	int hammingWeight(uint32_t n) {
+		return 0;
+	}
+};
+
+void test11() {
+	int n = 30;
+	Solution ss;
+	uint32_t oo = 1011;
+	//int bb = ss.hammingWeight(n);
+	string kk = to_string(oo);
+	
+	cout << kk << endl;
+}
+
+int main() {
+
+	test11();
+	system("pause");
+	return 0;
+}
